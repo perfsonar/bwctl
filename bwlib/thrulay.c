1dbac4e042f3eee13d79bd36e5011805e4c89a06
boote <boote@2da7661c-602c-0410-9277-d9994c0c2ef1>
2008-01-11 16:27:29 +0000

    Adding nuttcp.c and thrulay.c tool abstractions.
    git-svn-id: https://svn.internet2.edu/svn/bwctl/branches/BWL06CLEANUP@415 2da7661c-602c-0410-9277-d9994c0c2ef1

---
+++ bwlib/thrulay.c
@@ -0,0 +1,249 @@
+/*
+ * ex: set tabstop=4 ai expandtab softtabstop=4 shiftwidth=4:
+ * -*- mode: c-basic-indent: 4; tab-width: 4; indent-tabs-mode: nil -*-
+ *      $Id$
+ */
+/************************************************************************
+*                                                                       *
+*                           Copyright (C)  2007                         *
+*                               Internet2                               *
+*                           All Rights Reserved                         *
+*                                                                       *
+************************************************************************/
+/*
+ *    File:         thrulay.c
+ *
+ *    Author:       Jeff W. Boote
+ *                  Internet2
+ *
+ *    Date:         Mon Sep 03 12:42:26 MDT 2007
+ *
+ *    Description:    
+ *
+ *    This file encapsulates the functionality required to run the
+ *    thrulay throughput tool in bwctl.
+ *
+ *    This file will only be complied in by 'config' if TOOL_THRULAY
+ *    is defined.
+ */
+#include <bwlib/bwlibP.h>
+#include <thrulay/client.h>
+#include <thrulay/server.h>
+
+/*
+ * Function:    ThrulayAvailable
+ * 
+ * Description:
+ *    This file will only be complied in by 'config' if TOOL_THRULAY
+ *    is defined. So... The available func always returns True.
+ *
+ * In Args:
+ * Out Args:
+ * Scope:
+ *
+ * Returns:
+ *
+ * Side Effect:
+ *
+ */
+static BWLBoolean
+ThrulayAvailable(
+        BWLContext          ctx     __attribute__((unused)),
+        BWLToolDefinition   tool    __attribute__((unused))
+        )
+{
+    return True;
+}
+
+static thrulay_opt_t thrulay_opt;
+
+/*
+ * Function:    ThrulayPreRunTest
+ *
+ * Description:    
+ *              Does all 'prep' work for running a thrulay test.
+ *
+ *              Returns a 'closure' pointer. NULL indicates
+ *              failure.
+ *              This 'closure' pointer is passed on to the ThrulayRunTest.
+ *
+ *              (closure pointer is just non-null value that is not used)
+ * In Args:    
+ *
+ * Out Args:    
+ *
+ * Scope:    
+ * Returns:    
+ * Side Effect:    
+ */
+static void *
+ThrulayPreRunTest(
+        BWLContext          ctx,
+        BWLTestSession      tsess
+        )
+{
+    char    recvhost[MAXHOSTNAMELEN];
+    size_t  hlen;
+    int     rc;
+
+    hlen = sizeof(recvhost);
+    I2AddrNodeName(tsess->test_spec.receiver,recvhost,&hlen);
+    if(!hlen){
+        BWLError(tsess->cntrl->ctx,BWLErrFATAL,EINVAL,
+                "ThrulayPreRunTest: Invalid receiver I2Addr");
+        return NULL;
+    }
+
+
+    if(tsess->conf_receiver){
+        /* Run thrulay server through its API */
+
+        /* -w window size in bytes */
+        if(!tsess->test_spec.window_size){
+            tsess->test_spec.window_size = THRULAY_DEFAULT_WINDOW;
+        }
+
+        /* -m parallel test streams */
+        if(tsess->test_spec.parallel_streams < 1){
+            tsess->test_spec.parallel_streams = 1;
+        }
+
+        /* Log through stderr and verbose reports. */
+        rc = thrulay_server_init(LOGTYPE_STDERR,1);
+        if (rc < 0){
+            BWLError(ctx,BWLErrFATAL,errno,"Initializing thrulay server: %s",
+                    thrulay_server_strerror(rc));
+        }
+
+        fprintf(tsess->localfp,"bwctl: thrulay_server\n");
+    }
+    else{
+        /* Run thrulay client through its API */
+
+        /* Give default values to the test spec struct */
+        thrulay_client_options_init(&thrulay_opt);
+        /* But disable output. */
+        thrulay_opt.reporting_verbosity = -1;
+
+        if( !(thrulay_opt.server_name = strdup(recvhost))){
+            BWLError(ctx,BWLErrFATAL,errno,"ThrulayPreRunTest():strdup(): %M");
+            return NULL;
+        }
+
+        /* -t test duration in seconds */
+        thrulay_opt.test_duration = tsess->test_spec.duration;
+
+        /* -i reporting interval in seconds */
+        if(tsess->test_spec.report_interval){
+            thrulay_opt.reporting_interval = tsess->test_spec.report_interval;
+        }
+
+        /* -w window size in bytes */
+        if(tsess->test_spec.window_size){
+            thrulay_opt.window = tsess->test_spec.window_size;
+        }
+
+        /* -l block size */
+        if(tsess->test_spec.len_buffer){
+            thrulay_opt.block_size = tsess->test_spec.len_buffer;
+        }
+
+        /* -p server port */
+        thrulay_opt.port = tsess->tool_port;
+
+        /* Rate, if UDP test */
+        if(tsess->test_spec.udp){
+            thrulay_opt.rate = tsess->test_spec.bandwidth;
+        }
+
+        /* -m parallel test streams */
+        if(tsess->test_spec.parallel_streams > 0){
+            thrulay_opt.num_streams = tsess->test_spec.parallel_streams;
+        }
+
+        /* -b (busy wait in UDP test) and -D (DSCP value for TOS
+           byte): not used for the moment. */
+        /* Multicast options not used too. */
+
+        fprintf(tsess->localfp,"bwctl: thrulay_client\n");
+    }
+
+    return (void *)&thrulay_opt;
+}
+
+/*
+ * Function:    ThrulayRunTest
+ *
+ * Description:    
+ *
+ * In Args:    
+ *
+ * Out Args:    
+ *
+ * Scope:    
+ * Returns:    
+ * Side Effect:    
+ */
+static BWLBoolean
+ThrulayRunTest(
+        BWLContext          ctx,
+        BWLTestSession      tsess,
+        void                *closure    __attribute__((unused))
+        )
+{
+    int rc;
+
+    if(tsess->conf_receiver){
+        /*
+         * Now run thrulay server!
+         */
+        rc = thrulay_server_listen(tsess->tool_port,
+                tsess->test_spec.window_size);
+        if (rc < 0){
+            BWLError(ctx,BWLErrINFO,errno,"Thrulay server listen: %s", 
+                    thrulay_server_strerror(rc));
+        }
+        rc = thrulay_server_start(tsess->test_spec.parallel_streams, NULL);
+        if (rc < 0){
+            BWLError(ctx,BWLErrINFO,errno,"Thrulay server: %s", 
+                    thrulay_server_strerror(rc));
+        }
+        exit(0);
+    }
+    else{
+        /*
+         * Now run thrulay client!
+         */
+        rc = thrulay_client_init(thrulay_opt);
+        if (rc < 0){
+            BWLError(ctx,BWLErrINFO,errno,"Initializing thrulay client: %s",
+                    thrulay_client_strerror(rc));
+        }
+
+        rc = thrulay_client_start();
+        if (rc < 0){
+            BWLError(ctx,BWLErrINFO,errno,"While performing thrulay "
+                    "test: %s", thrulay_client_strerror(rc));
+        }
+        rc = thrulay_client_report_final();
+        if (rc < 0){
+            BWLError(ctx,BWLErrINFO,errno,"While generating thrulay"
+                    " final report: %s", thrulay_client_strerror(rc));
+        }
+        thrulay_client_exit();
+        exit(0);
+    }
+}
+
+
+BWLToolDefinitionRec    BWLToolThrulay = {
+    "thrulay",              /* name             */
+    NULL,                   /* def_cmd          */
+    NULL,                   /* def_server_cmd   */
+    5003,                   /* def_port         */
+    _BWLToolGenericParse,    /* parse            */
+    ThrulayAvailable,       /* tool_avail       */
+    _BWLToolGenericInitTest, /* init_test        */
+    ThrulayPreRunTest,      /* pre_run          */
+    ThrulayRunTest          /* run              */
+};
