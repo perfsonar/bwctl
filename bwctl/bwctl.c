1498348564f866d2c84cc80e7dd3742b4e8eaedb
aaron <aaron@2da7661c-602c-0410-9277-d9994c0c2ef1>
2008-12-17 20:28:31 +0000

    Use the recently added convenience function I2AddrIsLoopback instead of rolling my own.
    git-svn-id: https://svn.internet2.edu/svn/bwctl/trunk@502 2da7661c-602c-0410-9277-d9994c0c2ef1

--- bwctl/bwctl.c
+++ bwctl/bwctl.c
@@ -1999,7 +1999,7 @@ AGAIN:
             }
         }
 
-        if (BWLControlIsLoopback(first.cntrl) && !BWLControlIsLoopback(second.cntrl)) {
+        if (I2AddrIsLoopback(BWLAddrByControl(first.cntrl)) && !I2AddrIsLoopback(BWLAddrByControl(second.cntrl))) {
             char        buf[NI_MAXHOST+1];
             size_t      buflen = sizeof(buf);
             I2Addr new_addr = BWLAddrByLocalControl(second.cntrl);
@@ -2015,7 +2015,7 @@ AGAIN:
 
             I2ErrLog(eh,"Hostname '%s' resolves to a loopback address, using %s.", first.host, I2AddrNodeName(new_addr,buf,&buflen));
         }
-        else if (!BWLControlIsLoopback(first.cntrl) && BWLControlIsLoopback(second.cntrl)) {
+        else if (!I2AddrIsLoopback(BWLAddrByControl(first.cntrl)) && I2AddrIsLoopback(BWLAddrByControl(second.cntrl))) {
             char        buf[NI_MAXHOST+1];
             size_t      buflen = sizeof(buf);
             I2Addr new_addr = BWLAddrByLocalControl(first.cntrl);
