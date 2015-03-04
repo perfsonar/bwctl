6133c7ac8d71f3fdd843ccb261a08652002fe9b5
Hakan Calim <Hakan.Calim@fau.de>
2015-03-04 19:02:54 +0100

    solving conflict problems with git

--- tests/test_limitdb_creator.py
+++ tests/test_limitdb_creator.py
@@ -7,27 +7,29 @@ import unittest
 from nose.tools import eq_
 from nose.tools import ok_
 
-from os.path import dirname, realpath, sep, pardir
-import sys
-sys.path.append(dirname(realpath(__file__)) + sep + pardir + sep + "lib")
+from bwctl.server.limits_parser import *
 
-from bwctl.server import limits_parser
-
+sep = "/"
 limits_examples_folder = "limits_exampes"
 limits_big_file = "bwctld-big.limits"
 limits_simple_file = "bwctld-simple.limits"
+Limits_new = "bwctl_new_limits.conf"
 test_simple_file = limits_examples_folder + sep + limits_simple_file
 test_big_file = limits_examples_folder + sep + limits_big_file
+test_new_limits_file = limits_examples_folder + sep + Limits_new
 
 class Test(unittest.TestCase):
 
 
     def test_crator_with_simple_file(self):
-        ldbc = limits_parser.LimitsDBfromFileCreator(test_simple_file)
+        ldbc = LimitsDBfromFileCreator(test_simple_file)
         ldbc.create()
-        print ldbc.get_limits_classes()
               
-
+    def test_with_limitsv2(self):
+	lfpv2 = LimitFileParserV2(test_new_limits_file)
+	lfpv2.parse()
+	expected_result = 3
+        eq_(expected_result, lfpv2.get_limits_counter())
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
