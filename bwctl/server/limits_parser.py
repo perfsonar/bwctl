d10d08be5bc0fdef6073e262bc894e4cc7c3a1bb
Hakan Calim <Hakan.Calim@fau.de>
2015-04-07 17:29:46 +0200

    Changing v2 limits parser module

--- bwctl/server/limits_parser.py
+++ bwctl/server/limits_parser.py
@@ -19,8 +19,6 @@ class LimitsParser(object):
     def __init__(self, limits_file_path):
         self.pattern_get_assigns = 'assign.*\n'
         self.limits_as_string = ""                
-        self.limits_counter = 0 #Used for testing
-        self.assign_counter = 0
         self.limit_classes = {}
         self.open_limits_file(limits_file_path)        
         
@@ -37,13 +35,10 @@ class LimitsParser(object):
             raise
 
     def parse(self):
+        print "Start parsing..."
         self.parse_limits()
         self.parse_assign()
-            
-    def get_num_of_limit_classes(self):
-        return len(self.limit_classes)
-    def get_limits_counter(self):
-        return self.limits_counter
+    
     def parse_assign(self):
         assigns = re.findall(self.pattern_get_assigns, self.limits_as_string)
         self.assign_counter = len(assigns)
@@ -84,117 +79,6 @@ class LimitsParser(object):
 
 class LimitsFileParser(LimitsParser):
     '''
-    Parse the limits file  into a Dictionary. This can be used in the LimitsDB data structure.
-    Use it like:
-        lfb = LimitsFileParser(path_to_limit_file)
-    '''
-
-    def __init__(self, limits_file_path=""):
-        self.pattern_get_limits = '^limit \w+ with.*\n(?:\s+\w+.*\n){0,}'
-        self.pattern_limit_is_child = 'parent=(\w+)'
-	super(LimitsFileParser, self).__init__(limits_file_path)
-        self.limit_types_syntax = { "allow_open_mode" : ["on","off"],
-                                   "allow_tcp": ["on","off"],
-                                   "allow_udp": ["on","off"],
-                                   "bandwidth": ["int"],
-                                   "duration": ["int"],
-                                   "event_horizon" : ["int"],
-                                   "max_time_error" : ["int"],
-                                   "pending" : ["int"],
-                                   "parent" : ["string"],
-                                   }
-        
-    
-        
-    def parse_limits(self):
-        limits = re.findall(self.pattern_get_limits, self.limits_as_string, re.M)
-        if len(limits) < 1:
-            raise Exception("No Limits defined in format 1.x ")
-        limit_classes = {}
-        self.limits_counter = len(limits)
-        for limit in limits:
-             new_limit = re.sub(r'\\\n','',limit,re.M)
-             new_limit = re.sub(r'\n','',new_limit,re.M)
-             m = re.search(r'limit (\w+) with\s+(\w+=\w+),',new_limit)
-             if m:
-                 new_limit = re.sub(r'\s+','',new_limit)
-                 limit_types = new_limit.split(',')
-                 del limit_types[0] #delete head with limit class_name with..
-                 limit_types.append(m.group(2)) #append parameter which is in head
-                 limit_classes[m.group(1)] = {"LIMITTYPES" : limit_types}
-                 limit_classes[m.group(1)]["PARENT"] =  self.class_has_parent(limit_types, limit_classes)
-		 limit_classes[m.group(1)]["ASSIGN"] = {}
-                 
-        
-        self.limit_classes = limit_classes.copy()
-                
-    
-    def limit_types_syntax_check(self, limit_type):
-        '''
-        Checks if types are correct in limits file.
-        '''
-        match = re.search(r'(\w+)=(\w+)', limit_type)
-        if match:
-            if not match.group(1) in self.limit_types_syntax:
-               raise Exception("This limit type: %s is not alloed" % match.group(1))
-            elif not self.check_limit_type_value(match.group(1), match.group(2)):
-                raise Exception("Syntax check of limit type: %s fails" % limit_type)
-            
-                    
-            
-    def check_limit_type_value(self,limit_type, limit_type_value):
-        retval = None
-        value = self.limit_types_syntax[limit_type]
-        if "string" in value:
-            if type(limit_type_value) == type(""):
-                retval = 1
-        elif "int" in value:
-            # We can have values 50m
-            # AB: this doesn't seem to handle the "50m" case noted above
-            match =  re.search(r'(\d+)\S*', limit_type_value)
-            if type(int(match.group(1))) == type(1):
-                retval = 1
-        else:
-            if limit_type_value in value:
-                retval = 1
-                
-        return retval
-            
-            
-    def class_has_parent(self, limit_types, limit_classes):
-        for limit_type in limit_types:
-            #check first if limit_type is correct syntax
-            self.limit_types_syntax_check(limit_type)
-            match =  re.search(r'parent=(\w+)', limit_type)
-            if match:
-                #Syntax check if aprent exist
-                if match.group(1) in limit_classes:
-                    return match.group(1) #parent class name
-                else:
-                    raise Exception('Class name: %s does not exist! Please define it parent first.' % match.group(1))
-        return None
-    
-
-    # AB: is there a reason to return a number instead of just returning the
-    # limits themselves, and having the caller do "len"?
-    def get_num_of_limit_assigns(self, class_name):
-	return len(self.limit_classes[class_name]['ASSIGN']['net']) # at the moment it returns only net entries
-
-    def get_class_parent(self,class_name=None):
-        if class_name:
-            return self.limit_classes[class_name]["PARENT"]
-        else:
-            raise Exception('Please define a limit class name!')
-        
-    def get_num_of_limit_types(self, class_name=None):
-        if class_name:
-            return len(self.limit_classes[class_name]["LIMITTYPES"])
-        else:
-            raise Exception('Please define a limit class name!')
-        
-
-class LimitFileParserV2(LimitsParser):
-    '''
     This class parsers limits files built on version 2 syntaxt. The limits file has the form like example below:
     <class "root_users">
     # No parent class
@@ -224,7 +108,7 @@ class LimitFileParserV2(LimitsParser):
     '''
     def __init__(self, limits_file_path):
         self.pattern_get_limits = r'<class \S+>.*\s*(?:parent.*){0,1}\s*.*(?:<.*limits.*>(?:\s*\w*\s*\w*\s*){0,}.*\s*</.*limits>\s*){1,}</class>' 
-	super(LimitFileParserV2, self).__init__(limits_file_path)
+        super(LimitsFileParser, self).__init__(limits_file_path)
           
     def parse_limits(self):
         self.limits_as_string = re.sub('#.*\\n',"",self.limits_as_string) #remove all comments first
@@ -298,27 +182,22 @@ class LimitsDBfromFileCreator(object):
         self.limits_db = LimitsDB()
         self.limits_file_path = limits_file_path
         self.limits_classes = {}
-    def create(self):
+    def get_limitsdb(self):
         '''
-        Try to create first limits db from legacy file. If it fails it tries to 
-        to crate with version 2  format limits file format parser.
+        Creaes from limits file an limits db data structure.
         '''
 	# AB: I'm not sure it makes sense to support the v1 parsing as anything
 	# more than an upgrade script, so it may make more sense to axe the v1
 	# parsing in here, and only support creating a limits db from v2. We'd
 	# include an "upgrade_limits.py" script that would convert the v1
 	# limits into a v2 limits file.
-        lfp = None
-        try:
-            lfp = LimitsFileParser(self.limits_file_path)
-            lfp.parse()
-        except Exception:
-            lfp = LimitFileParserV2(self.limits_file_path)
-            lfp.parse()
-
         
+        lfp = LimitsFileParser(self.limits_file_path)
+        lfp.parse()
+                
         self.limits_classes = lfp.get_limits_classes()
         self.create_limitsdb()
+        return self.limits_db
     def create_limitsdb(self):
         limit_classes = self.limits_classes
         success_class_name = []
@@ -338,15 +217,22 @@ class LimitsDBfromFileCreator(object):
         for del_class_name in success_class_name:
             del(limit_classes[del_class_name])              
         
-    def add_all_class_elements(self, limit_classes, class_name):         
-        networks = []
+    def add_all_class_elements(self, limit_classes, class_name):
         limit_types = limit_classes[class_name]['LIMITTYPES']
+<<<<<<< HEAD
+=======
         if 'net' in limit_classes[class_name]['ASSIGN']:
             networks = limit_classes[class_name]['ASSIGN']['net'] 
         if 'network' in limit_classes[class_name]['ASSIGN']:
             networks = limit_classes[class_name]['ASSIGN']['network'] 
+>>>>>>> 1c31329fe5669b3240c0932738ae38bada70f165
         self.add_limits_types_to_limitsdb(class_name, limit_types)
-        self.add_class_network(class_name, networks)
+        if 'network' in limit_classes[class_name]['ASSIGN']:
+            print "Adding networks"
+            self.add_class_network(class_name, limit_classes[class_name]['ASSIGN']['network'])
+        elif 'user' in limit_classes[class_name]['ASSIGN']:
+            print "Adding users"
+            self.add_class_user(class_name, limit_classes[class_name]['ASSIGN']['user'])
         
     def add_limits_types_to_limitsdb(self, class_name, limit_types):
         limit_type_class = None
@@ -376,9 +262,18 @@ class LimitsDBfromFileCreator(object):
     def add_class_network(self, class_name, networks):
         for network in networks:
             self.limits_db.add_network(network, class_name)
+    def add_class_user(self, class_name, users):
+        for user in users:
+            self.limits_db.add_user(user, class_name)
             
     def get_limits_classes(self):
-        return  self.limits_db.get_limits()           
+        return  self.limits_db.get_limits()
+    
+
+def create_limitsdb(limits_file_path):
+    ldbc = LimitsDBfromFileCreator(limits_file_path)
+    return ldbc.get_limitsdb()
+              
 
              
 
